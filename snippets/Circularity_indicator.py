0d73d9300bb8e09639231ed84072f6136d5620d2
ArturTomczak <artomczak@gmail.com>
2022-07-26 10:09:15 +0200

    add demo circularity calculator

--- snippets/Circularity_indicator.py
+++ snippets/Circularity_indicator.py
@@ -1,9 +1,24 @@
 
-revit_materials = IN[0]
+elements = IN[0]
 
 # turn into list of lists:
-if not isinstance(revit_materials[0], list):
-    revit_materials = [revit_materials] 
+if not isinstance(elements[0], list):
+    elements = [elements] 
 
+result = []
 
-OUT = True
+volumes = [el[4] for el in elements]
+
+#replace nans with -1
+volumes = [-1 if x == '' else x for x in volumes]
+
+max_volume = max(volumes)
+
+for v in volumes:
+    if v>0:
+        result.append(v/max_volume)
+    else:
+        result.append(-1)
+
+# OUT = result
+OUT = result
