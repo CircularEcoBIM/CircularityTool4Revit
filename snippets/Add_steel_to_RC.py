5adc6f5b2a89127cff92988e02ab3267eeae9472
ArturTomczak <artomczak@gmail.com>
2022-09-01 15:33:05 +0200

    update
    exclude snippet
    improve template excel
    fix bugs

---
+++ snippets/Add_steel_to_RC.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+
+original_materials = IN[0]
+materials = IN[1]
+volumes = IN[2]
+steel_concrete_ratio = IN[3]
+
+new_materials = deepcopy(materials)
+org_materials = deepcopy(original_materials)
+new_volumes = deepcopy(volumes)
+
+for i in range(len(materials)):
+    for j in range(len(materials[i])):
+        if materials[i][j] == "Concrete":
+            new_volumes[i][j] = volumes[i][j] * (1-steel_concrete_ratio)
+            if "Steel" in materials[i]:
+                steel_id = materials[i].index("Steel")
+                new_volumes[i][steel_id] = volumes[i][j]*steel_concrete_ratio
+            else:
+                new_materials[i].append("Steel")
+                new_volumes[i].append(volumes[i][j]*steel_concrete_ratio)
+                org_materials[i].append("<added Steel>")
+
+OUT = [new_materials, new_volumes, org_materials]
--- snippets/Disassembly_indicator.py
+++ snippets/Disassembly_indicator.py
@@ -13,22 +13,22 @@ if not isinstance(parameters[0], list):
 dfd_ind = []
 
 for el in parameters:
-    if not el[0]:
+    if not el[0] or el[0] > 5:
         el[0] = 5
     d1 = values["Connection type"][str(int(el[0]))]
     
-    if not el[1]:
+    if not el[1] or el[1] > 4:
         el[1] = 4
     d2 = values["Connection accessibility"][str(int(el[1]))]
     
-    if not el[2]:
+    if not el[2] or el[2] > 3:
         el[2] = 3
     d3 = values["Crossings"][str(int(el[2]))]
 
-    if not el[3]:
+    if not el[3] or el[3] > 4:
         el[3] = 4
     d4 = values["Form containment"][str(int(el[3]))]
 
-    dfd_ind.append(d1+d2+d3+d4)
+    dfd_ind.append([d1,d2,d3,d4])
 
 OUT = dfd_ind
